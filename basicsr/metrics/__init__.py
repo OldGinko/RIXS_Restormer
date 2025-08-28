738b477c7cf3f01f459e182d70f01189911ba093
OldGinko <you@example.comugur.gueltekin@uzh.ch>
2025-08-28 07:35:43 +0200

    Add MAE and MSE loss functions to metrics and update exports in respective modules

--- basicsr/metrics/__init__.py
+++ basicsr/metrics/__init__.py
@@ -1,4 +1,5 @@
 from .niqe import calculate_niqe
 from .psnr_ssim import calculate_psnr, calculate_ssim
+from .mae_mse import calculate_mae, calculate_mse
 
-__all__ = ['calculate_psnr', 'calculate_ssim', 'calculate_niqe']
+__all__ = ['calculate_psnr', 'calculate_ssim', 'calculate_niqe', 'calculate_mae', 'calculate_mse']
--- basicsr/models/losses/__init__.py
+++ basicsr/models/losses/__init__.py
@@ -1,5 +1,7 @@
 from .losses import (L1Loss, MSELoss, PSNRLoss, CharbonnierLoss)
+from .target_mae_mse_loss import (TargetMAEMSELoss, SimpleTargetMSELoss, SimpleTargetMAELoss)
 
 __all__ = [
     'L1Loss', 'MSELoss', 'PSNRLoss', 'CharbonnierLoss',
+    'TargetMAEMSELoss', 'SimpleTargetMSELoss', 'SimpleTargetMAELoss',
 ]
